56b0dbb9f6b001c8315c7345e6c5ab31283cd202
munisisazade <munisisazade@gmail.com>
2024-08-11 23:13:28 +0200

    New version relased #7

--- redisrpc/errors.py
+++ redisrpc/errors.py
@@ -1,10 +1,13 @@
-
-
 class Handler(Exception):
     def __init__(self, message, errors):
-
         # Call the base class constructor with the parameters it needs
         super().__init__(message)
 
         # Now for your custom code...
         self.errors = errors
+
+
+class ChannelActiveException(Exception):
+    def __init__(self, channel):
+        self.channel = channel
+        super().__init__(f"Channel '{channel}' is currently active.")
